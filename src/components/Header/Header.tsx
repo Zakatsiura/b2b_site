b59a9a7e9fe77df5bbc43949c0b697119270ddd3
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-05-28 20:25:50 +0300

    init2

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,10 +5,18 @@ import style from './Header.module.css';
 const Header = () => {
     return (
         <div className={style.header}>
-            <Link to="/main">Main</Link>
-            <Link to="/aircompany">Air Company</Link>
-            <Link to="/service">Service</Link>
-            <Link to="/contact">Contact</Link>
+            <Link to="/main" className={style.header__link}>
+                Main
+            </Link>
+            <Link to="/aircompany" className={style.header__link}>
+                Air Company
+            </Link>
+            <Link to="/service" className={style.header__link}>
+                Service
+            </Link>
+            <Link to="/contact" className={style.header__link}>
+                Contact
+            </Link>
         </div>
     );
 };
