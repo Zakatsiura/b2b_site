c97761aa89f999f57522253ed7f99f89fc583104
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-07-15 12:13:15 +0300

    Ajust ScrollToTop in height

--- src/components/ScrollToTopButton/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton/ScrollToTopButton.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import style from './ScrollToTopButton.module.css';
 import arrow from '../../assets/icons/arrow-top.png';
 
 const ScrollToTopButton: React.FC = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollTop =
+                window.scrollY || document.documentElement.scrollTop;
+            const scrollThreshold = 500;
+
+            setIsVisible(scrollTop > scrollThreshold);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     const scrollToTop = () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     };
 
-    return (
-        <img className={style.top} src={arrow} onClick={scrollToTop}>
-        </img>
-    );
+    return isVisible ? (
+        <img className={style.top} src={arrow} onClick={scrollToTop} />
+    ) : null;
 };
 
 export { ScrollToTopButton };
