cb0072530458c0a01725671d8cc7b42f6f0b2e4f
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-07-15 14:40:07 +0300

    Modify footer with links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,32 +5,54 @@ import instagram from '../../assets/icons/instagram-icon.png';
 
 const Footer = () => {
     return (
-        <div className={style.footer__wrapper}>
-            <div className={style.footer__text}>
-                © B2B Aviation Trading 2023
+        <div className={style.footer}>
+            <div className={style.footer__nav}>
+                <ul className={style.footer__nav_list}>
+                    <li className={style.footer__nav_item}>
+                        <a href="/main">About</a>
+                    </li>
+                    <li className={style.footer__nav_item}>
+                        <a href="/aircompany">Air Company</a>
+                    </li>
+                    <li className={style.footer__nav_item}>
+                        <a href="/service">Service</a>
+                    </li>
+                    <li className={style.footer__nav_item}>
+                        <a href="/contact">Contacts</a>
+                    </li>
+                    <li className={style.footer__nav_item}>
+                        <a href="/certificates">Certificates</a>
+                    </li>
+                </ul>
             </div>
-            <div className={style.footer__icon__block}>
-                <a href="https://www.facebook.com/" target="blank">
-                    <img
-                        src={facebook}
-                        className={style.footer__icon}
-                        alt="fb icon"
-                    />
-                </a>
-                <a href="https://www.linkedin.com/" target="blank">
-                    <img
-                        src={linkedin}
-                        className={style.footer__icon}
-                        alt="linkedin icon"
-                    />
-                </a>
-                <a href="https://www.instagram.com/" target="blank">
-                    <img
-                        src={instagram}
-                        className={style.footer__icon}
-                        alt="instargam icon"
-                    />
-                </a>
+            <div className={style.footer__divider}></div>
+            <div className={style.footer__wrapper}>
+                <div className={style.footer__text}>
+                    © B2B Aviation Trading 2023
+                </div>
+                <div className={style.footer__icon__block}>
+                    <a href="https://www.facebook.com/" target="blank">
+                        <img
+                            src={facebook}
+                            className={style.footer__icon}
+                            alt="fb icon"
+                        />
+                    </a>
+                    <a href="https://www.linkedin.com/" target="blank">
+                        <img
+                            src={linkedin}
+                            className={style.footer__icon}
+                            alt="linkedin icon"
+                        />
+                    </a>
+                    <a href="https://www.instagram.com/" target="blank">
+                        <img
+                            src={instagram}
+                            className={style.footer__icon}
+                            alt="instargam icon"
+                        />
+                    </a>
+                </div>
             </div>
         </div>
     );
