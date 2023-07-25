40b50db280ddacd883ea5dc2a46b46a9dacb5bfb
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-07-25 16:59:17 +0300

    Refactore array

--- src/components/Certificates/Certificates.tsx
+++ src/components/Certificates/Certificates.tsx
@@ -5,14 +5,26 @@ import styles from './Certificates.module.css';
 import { LanguageContext } from '../../context/languageContext';
 import { ScrollToTopButton } from '../ScrollToTopButton/ScrollToTopButton';
 import { locales } from '../../locales/locales';
-import image1 from '../../assets/certificates/MRO_Airbus.png';
-import image2 from '../../assets/certificates/AOC.png';
-import image3 from '../../assets/certificates/MOAC.png';
-import image4 from '../../assets/certificates/MOAC_1.png';
-import image5 from '../../assets/certificates/CAMO.png';
-import image6 from '../../assets/certificates/CAMO_1.png';
-import image7 from '../../assets/certificates/DIST_en.png';
-import image8 from '../../assets/certificates/DIST_ua.png';
+
+import MRO_Airbus from '../../assets/certificates/MRO_Airbus.png';
+import AOC from '../../assets/certificates/AOC.png';
+import MOAC from '../../assets/certificates/MOAC.png';
+import MOAC_1 from '../../assets/certificates/MOAC_1.png';
+import CAMO from '../../assets/certificates/CAMO.png';
+import CAMO_1 from '../../assets/certificates/CAMO_1.png';
+import DIST_en from '../../assets/certificates/DIST_en.png';
+import DIST_ua from '../../assets/certificates/DIST_ua.png';
+
+const imagePaths = [
+    MRO_Airbus,
+    AOC,
+    MOAC,
+    MOAC_1,
+    CAMO,
+    CAMO_1,
+    DIST_en,
+    DIST_ua,
+];
 
 const Certificates = () => {
     const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -41,78 +53,24 @@ const Certificates = () => {
             <h2 className={styles.title}>
                 {locales[language].certificates__titile}
             </h2>
-            <div className={styles.imageRow}>
-                <div className={styles.imageContainer}>
-                    <img
-                        src={image1}
-                        alt="MRO certificate"
-                        className={styles.image}
-                        onClick={() => openModal(image1)}
-                    />
-                </div>
-                <div className={styles.imageContainer}>
-                    <img
-                        src={image2}
-                        alt="AOC certificate"
-                        className={styles.image}
-                        onClick={() => openModal(image2)}
-                    />
-                </div>
-            </div>
-            <div className={styles.imageRow}>
-                <div className={styles.imageContainer}>
-                    <img
-                        src={image3}
-                        alt="MOAC certificate"
-                        className={styles.image}
-                        onClick={() => openModal(image3)}
-                    />
-                </div>
-                <div className={styles.imageContainer}>
-                    <img
-                        src={image4}
-                        alt="MOAC_1 certificate"
-                        className={styles.image}
-                        onClick={() => openModal(image4)}
-                    />
-                </div>
-            </div>
-            <div className={styles.imageRow}>
-                <div className={styles.imageContainer}>
-                    <img
-                        src={image5}
-                        alt="CAMO certificate"
-                        className={styles.image}
-                        onClick={() => openModal(image5)}
-                    />
-                </div>
-                <div className={styles.imageContainer}>
-                    <img
-                        src={image6}
-                        alt="CAMO_1 certificate"
-                        className={styles.image}
-                        onClick={() => openModal(image6)}
-                    />
-                </div>
-            </div>
-            <div className={styles.imageRow}>
-                <div className={styles.imageContainer}>
-                    <img
-                        src={image7}
-                        alt="DIST_en  certificate"
-                        className={styles.image}
-                        onClick={() => openModal(image7)}
-                    />
-                </div>
-                <div className={styles.imageContainer}>
-                    <img
-                        src={image8}
-                        alt="DIST_ua certificate"
-                        className={styles.image}
-                        onClick={() => openModal(image8)}
-                    />
-                </div>
-            </div>
+            {Array.from({ length: Math.ceil(imagePaths.length / 2) }).map(
+                (_, index) => (
+                    <div key={index} className={styles.imageRow}>
+                        {imagePaths
+                            .slice(index * 2, index * 2 + 2)
+                            .map((imagePath, i) => (
+                                <div key={i} className={styles.imageContainer}>
+                                    <img
+                                        src={imagePath}
+                                        alt={`Certificate ${index * 2 + i + 1}`}
+                                        className={styles.image}
+                                        onClick={() => openModal(imagePath)}
+                                    />
+                                </div>
+                            ))}
+                    </div>
+                )
+            )}
             <Modal
                 isOpen={modalIsOpen}
                 onRequestClose={closeModal}
@@ -123,7 +81,7 @@ const Certificates = () => {
                 {selectedImage && (
                     <img
                         src={selectedImage}
-                        alt="Enlarged Image"
+                        alt="Enlarged Certificate"
                         className={styles.enlargedImage}
                         onClick={closeModal}
                     />
