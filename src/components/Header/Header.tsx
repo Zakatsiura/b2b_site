d438f2668fd9432e197cd41a9a50b899ee6da873
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-07-26 22:55:30 +0300

    Add switcher text

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,23 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 
 import style from './Header.module.css';
 import menu from '../../assets/images/burger-menu-icon.jpg';
 import logo_small from '../../assets/images/logo_small.png';
+import { LanguageContext } from '../../context/languageContext';
+import { locales } from '../../locales/locales';
 import { Menu } from '../Menu/Menu';
 import { Link } from 'react-router-dom';
 import { LanguageSwitcher } from '../LanguageSwitcher/LanguageSwitcher';
 
 const Header = () => {
+    const languageContext = useContext(LanguageContext);
     const [isOpen, setIsOpen] = useState(false);
+
+    if (!languageContext) {
+        return null;
+    }
+
+    const { language } = languageContext;
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
@@ -17,16 +26,27 @@ const Header = () => {
         <div className={style.header}>
             <div className={style.logo__block}>
                 <Link to={'/'} className={style.header__link}>
-                    <img src={logo_small} className={style.logo__small} alt='logo'/>
+                    <img
+                        src={logo_small}
+                        className={style.logo__small}
+                        alt="logo"
+                    />
                     <div className={style.header__link_text}>
-                        B<span className={style.logo__color_digit}>2</span>B Aviation Trading
+                        <span className={style.header__lint_text_cap}>
+                            B<span className={style.logo__color_digit}>2</span>B
+                        </span>{' '}
+                        Aviation Trading
                     </div>
                 </Link>
             </div>
             <div className={style.rightside}>
+                <span className={style.rightside__text}>
+                    {' '}
+                    {locales[language].header__switch}
+                </span>
                 <LanguageSwitcher />
                 <div className={style.header__button} onClick={toggleMenu}>
-                    <img src={menu} className={style.burger} alt='menu'/>
+                    <img src={menu} className={style.burger} alt="menu" />
                 </div>
                 <Menu isOpen={isOpen} onChange={setIsOpen} />
             </div>
