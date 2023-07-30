a5e9b98077760b0f9e630373d5a1db5ff6cdcb29
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-07-30 18:02:09 +0300

    Fix index in components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export * from './Camo/Camo';
 export * from './Card/Card';
 export * from './Certificates/Certificates';
 export * from './Contact/Contact';
@@ -8,6 +9,7 @@ export * from './LanguageSwitcher/LanguageSwitcher';
 export * from './Main/Main';
 export * from './MainSlider/MainSlider';
 export * from './Menu/Menu';
+export * from './Mro/Mro';
 export * from './Partners/Partners';
 export * from './ScrollToTopButton/ScrollToTopButton';
 export * from './Service/Service';
