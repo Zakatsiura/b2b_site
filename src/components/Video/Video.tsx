cb075d7d530ec714fdcd2565bf54bdf5850fb9b5
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-08-01 02:31:43 +0300

    Css

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useRef } from 'react';
 import { LanguageContext } from '../../context/languageContext';
-import { locales } from '../../locales/locales';
+// import { locales } from '../../locales/locales';
 
 import styles from './Video.module.css';
 import main from '../../assets/video/testvideo.mp4';
@@ -10,19 +10,14 @@ const Video: React.FC = () => {
     useEffect(() => {
         const videoElement = videoRef.current;
 
-        // Check if the video is already muted (in case the user already interacted with the page)
         if (videoElement && videoElement.muted) {
-            // Play the video programmatically to force autoplay on iPhones
             const playPromise = videoElement.play();
 
-            // Handle the promise returned by the play() method
             if (playPromise !== undefined) {
                 playPromise
                     .then(() => {
-                        // Autoplay started successfully
                     })
                     .catch((error) => {
-                        // Autoplay failed, you may need to display a play button or a user gesture to start the video
                     });
             }
         }
@@ -52,8 +47,8 @@ const Video: React.FC = () => {
                 </video>
                 <div className={styles.content}>
                     <h1 className={styles.content}>
-                        {locales[language].slogan}
-                        <p>test video</p>
+                        {/* {locales[language].slogan}
+                        <p>test video</p> */}
                     </h1>
                 </div>
             </section>
