0bddbcef81869382ce732d76aaf874d2b6ca74a6
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-08-01 02:21:29 +0300

    Fix IOS Video?

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,11 +1,15 @@
-import { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import { LanguageContext } from '../../context/languageContext';
 import { locales } from '../../locales/locales';
 
 import styles from './Video.module.css';
 import main from '../../assets/video/testvideo.mp4';
 
-const Video = () => {
+interface VideoProps {
+    // Добавьте необходимые пропсы для компонента, если есть
+}
+
+const Video: React.FC<VideoProps> = () => {
     const languageContext = useContext(LanguageContext);
 
     if (!languageContext) {
@@ -13,11 +17,40 @@ const Video = () => {
     }
 
     const { language } = languageContext;
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        // Автовоспроизведение с заглушенным звуком при загрузке компонента
+        if (videoRef.current) {
+            videoRef.current.muted = true;
+            videoRef.current.play().catch((error) => {
+                console.error('Autoplay error:', error);
+            });
+        }
+    }, []);
+
+    const handlePlay = () => {
+        // Включение звука при воспроизведении видео
+        if (videoRef.current) {
+            videoRef.current.muted = false;
+        }
+    };
+
     return (
         <>
             <section className={styles.main}>
                 <div className={styles.overlay}></div>
-                <video autoPlay loop muted className={styles.video}>
+                <video
+                    ref={videoRef}
+                    autoPlay
+                    loop
+                    muted
+                    playsInline // Включить автовоспроизведение на iOS
+                    className={styles.video}
+                    onError={(e) => {
+                        console.error('Video error:', e);
+                    }}
+                >
                     <source src={main} type="video/mp4" />
                 </video>
                 <div className={styles.content}>
